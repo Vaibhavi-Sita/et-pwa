c1da00356966baa07e666037753bfcb36f1c2f89
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2023-01-09 15:19:30 +0530

    code update

--- et-pwa/src/App.test.tsx
+++ et-pwa/src/App.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders about link', () => {
-  const { getByText } = render(<Link />);
+  const { getByText } = render(<App/>);
   const linkElement = getByText(/about/i);
   expect(linkElement).toBeInTheDocument();
 });
--- et-pwa/src/App.tsx
+++ et-pwa/src/App.tsx
@@ -1,29 +1,31 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-const About = React.lazy(() => import('./About'));
-const Home = React.lazy(() => import('./Home'));
+const About = lazy(() => import("./About"));
+const Home = lazy(() => import("./Home"));
 
 const App: React.FC = () => (
   <Router>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-    <Routes>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/">
-        <Home />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Routes>
+    </Suspense>
   </Router>
 );
+
 export default App;

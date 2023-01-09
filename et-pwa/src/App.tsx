ce097b40f8fe9de84e0c33db6ae4fb13af9e11b5
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2023-01-09 17:57:20 +0530

    ui update

--- et-pwa/src/App.tsx
+++ et-pwa/src/App.tsx
@@ -1,9 +1,11 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
-const About = lazy(() => import('./pages/About'));
-const Home = lazy(() => import('./pages/Home') );
-const Payment = lazy(() => import('./pages/Payment') );
+const About = lazy(() => import('./components/pages/About'));
+const Home = lazy(() => import('./components/pages/Home') );
+const Payment = lazy(() => import('./components/pages/Payment') );
+const Services = lazy(() => import('./components/pages/Services') );
+
 const App: React.FC = () => (
   <Router>
     <Suspense fallback={<div>Loading...</div>}>
@@ -15,9 +17,13 @@ const App: React.FC = () => (
         <li>
           <Link to="/about">About</Link>
         </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
       </ul>
     </nav>
     <Routes>
+      <Route path="/services" element={<Services />} />
       <Route path="/about" element={<About />} />
       <Route path="/" element={<Home />} />
     </Routes>

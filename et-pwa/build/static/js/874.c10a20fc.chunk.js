968afa2a57f086bd9c654358b736a92cc3015d62
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2023-01-09 15:50:56 +0530

    revert

--- et-pwa/build/static/js/874.c10a20fc.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunket_pwa=self.webpackChunket_pwa||[]).push([[874],{179:function(e,n,t){t.r(n);t(791);var u=t(184);n.default=function(){return(0,u.jsx)("h1",{children:"Eternal Tales welcomes you"})}}}]);
-//# sourceMappingURL=874.c10a20fc.chunk.js.map
